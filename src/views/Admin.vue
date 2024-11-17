7c188b881c4960a196a158e9433a72cb22428579
Sagar Makhija <sagarmakhija@Sagars-MacBook-Air.local>
2024-11-18 01:41:27 +0530

    WIP Admin

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="admin mt-4">
-    <pre>
-      {{ predictions }}
-    </pre>
     <ag-grid-vue
-      :rowData="rows"
       :columnDefs="cols"
+      :rowData="modifiedPredictions"
       style="height: 500px; width: 100%"
       class="ag-theme-quartz"
     />
@@ -13,26 +10,54 @@
 </template>
 
 <script lang="js">
-import 'ag-grid-community/styles/ag-grid.css' // Mandatory CSS required by the Data Grid
-import 'ag-grid-community/styles/ag-theme-quartz.css' // Optional Theme applied to the Data Grid
+import 'ag-grid-community/styles/ag-grid.css'
+import 'ag-grid-community/styles/ag-theme-quartz.css'
 import { AgGridVue } from 'ag-grid-vue3'
 
 export default {
   components: { AgGridVue },
+  watch: {
+    predictions() {
+      this.makeTableData()
+    },
+  },
   data() {
     return {
-      // users, location, item, count
-      cols: [{ field: 'make' }, { field: 'model' }, { field: 'price' }, { field: 'electric' }],
-      rows: [
-        { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
-        { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
-        { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
+      cols: [
+        { field: 'user' },
+        { field: 'location' },
+        { field: 'item' },
+        { field: 'count' },
+        { field: 'date' },
       ],
+      rows: [{ make: 'Tesla', model: 'Model Y', price: 64950, electric: true }],
+      modifiedPredictions: [],
     }
   },
   computed: {
     predictions() {
       return this.$store.getters.predictions
+    },
+    locations() {
+      return this.$store.getters.locations
+    },
+    users() {
+      return this.$store.getters.users
+    },
+    items() {
+      return this.$store.getters.items
+    },
+  },
+  methods: {
+    makeTableData() {
+      this.modifiedPredictions = this.predictions.map((pred) => {
+        return {
+          ...pred,
+          location: this.locations.find((l) => l.id == pred.locationid)?.name,
+          user: this.users.find((u) => u.id == pred.userid)?.name,
+          item: this.items.find((i) => i.id == pred.itemid)?.name,
+        }
+      })
     },
   },
 }
