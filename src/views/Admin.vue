4cc6409f1b791e37e2bbdb18dc5acd3db147fe07
Sagar Makhija <sagarmakhija@Sagars-MacBook-Air.local>
2024-11-18 10:11:23 +0530

    Table changes and show data by location

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="admin mt-4">
+    <div class="location my-2">
+      <select
+        v-model="location"
+        name=""
+        id=""
+        class="mt-1 block w-80 border border-gray-300 px-3 py-2 rounded-lg appearance-none"
+        @change="makeTableData"
+      >
+        <option :value="null" selected>Please Select the location</option>
+        <option v-for="location in locations" :key="location.docId" :value="location.id">
+          {{ location.name }}
+        </option>
+      </select>
+    </div>
     <ag-grid-vue
       :columnDefs="cols"
       :rowData="modifiedPredictions"
-      style="height: 500px; width: 100%"
+      style="width: 100%"
+      domLayout="autoHeight"
+      :autoSizeStrategy="autoSizeStrategy"
       class="ag-theme-quartz"
     />
   </div>
@@ -23,6 +39,7 @@ export default {
   },
   data() {
     return {
+      location: null,
       cols: [
         { field: 'user' },
         { field: 'location' },
@@ -32,6 +49,10 @@ export default {
       ],
       rows: [{ make: 'Tesla', model: 'Model Y', price: 64950, electric: true }],
       modifiedPredictions: [],
+      autoSizeStrategy: {
+        type: 'fitGridWidth',
+        defaultMinWidth: 100,
+      },
     }
   },
   computed: {
@@ -50,14 +71,21 @@ export default {
   },
   methods: {
     makeTableData() {
-      this.modifiedPredictions = this.predictions.map((pred) => {
-        return {
-          ...pred,
-          location: this.locations.find((l) => l.id == pred.locationid)?.name,
-          user: this.users.find((u) => u.id == pred.userid)?.name,
-          item: this.items.find((i) => i.id == pred.itemid)?.name,
-        }
-      })
+      this.modifiedPredictions = this.predictions
+        .map((pred) => {
+          if (pred?.date?.seconds) console.log(new Date(pred.date?.seconds * 1000))
+          return {
+            ...pred,
+            location: this.locations.find((l) => l.id == pred.locationid)?.name,
+            user: this.users.find((u) => u.id == pred.userid)?.name,
+            item: this.items.find((i) => i.id == pred.itemid)?.name,
+            date: new Date(pred.date?.seconds * 1000),
+          }
+        })
+        .filter((prediction) => {
+          if (!this.location) return prediction
+          return prediction.locationid == this.location
+        })
     },
   },
 }
