d8c27b659027c8d6e2751ed51b9befc92a5e0a9f
Sagarmak <118016295+Sagarmak@users.noreply.github.com>
2025-10-06 00:13:43 +0530

    Add new assets and fix styling in Home (#2)

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 // DB config
 
 export default {
+  year: 2025,
   location: 'location',
-  users: 'users',
-  items: 'items',
-  prediction: 'prediction',
+  users: 'users-2025',
+  items: 'items-2025',
+  prediction: 'prediction-2025',
 }
